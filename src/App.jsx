eb577389ba226427057e245cbb9d83ba0cc93431
VanMinh-HNIMNAVGNAD <vanminh00052@gmail.com>
2026-08-05 15:11:59 +0700

    feat(app): update App main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import MainLayout from './components/layout/MainLayout';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { FileProvider } from './context/FileContext';
+import AppRouter from './routes/AppRouter';
 
-function App() {
-  return (
-      <MainLayout>
-        <div>
-          <h2 className="text-2xl font-normal mb-6 text-gray-800">Drive của tôi</h2>
-          <div className="grid grid-cols-4 gap-4">
-            <div className="h-40 bg-gray-50 border border-gray-200 rounded-xl flex items-center justify-center text-gray-400">
-              Mock Folder/File
-            </div>
-          </div>
-        </div>
-      </MainLayout>
-  );
+export default function App() {
+    return (
+        <BrowserRouter>
+            <AuthProvider>
+                <FileProvider>
+                    <AppRouter />
+                </FileProvider>
+            </AuthProvider>
+        </BrowserRouter>
+    );
 }
-
-export default App;
